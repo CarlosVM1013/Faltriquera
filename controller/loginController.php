563f26eb587369beca375293e1281c059991c4bf
CarlosVM1013 <96196850+CarlosVM1013@users.noreply.github.com>
2022-01-16 20:10:26 +0100

    Update loginController.php

--- controller/loginController.php
+++ controller/loginController.php
@@ -34,7 +34,6 @@ function comprobarCredenciales() {
 
     $query = mysqli_query($link, 'SELECT * FROM users WHERE usuario="'.$usuario.'" ');
     $results = mysqli_fetch_array($query);
-    echo $results['password'];
     if ($results['password'] !== $contrasena) {
         mysqli_close($link);
         $valido = false;

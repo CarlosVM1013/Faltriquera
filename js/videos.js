71b36565d2aafb830e6b68a8da087525bf4f546a
Carlos <carlos.vicente.mellinas@gmail.com>
2022-07-20 21:26:37 +0200

    Se cambia el display del div de video por block

--- js/videos.js
+++ js/videos.js
@@ -36,12 +36,12 @@ function buscar() {
     $("#videos .video h4").each(function () {
       if (nombreVideo != "") {
         if ($(this).text().toLowerCase().includes(nombreVideo)) {
-          $(this).parent().css("display", "flex");
+          $(this).parent().css("display", "block");
         } else {
           $(this).parent().css("display", "none");
         }
       } else {
-        $("#videos .video").css("display", "flex");
+        $("#videos .video").css("display", "block");
       }
     });
   }

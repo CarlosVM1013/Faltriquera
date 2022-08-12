2c96d6b3b0089bf433bca348e26564fe0b70ec6d
Carlos <carlos.vicente.mellinas@gmail.com>
2022-08-13 00:03:28 +0200

    Actualizando usuario base de datos

--- controller/loginController.php
+++ controller/loginController.php
@@ -2,7 +2,7 @@
 
 function conexion() {
     $direccion = "127.0.0.1";
-    $usuario = "bitnami";
+    $usuario = "root";
     $contrasenya = "vIu5oSLpNMM7";
     $bd = "faltriquera";    
     $conexion=new mysqli($direccion, $usuario, $contrasenya, $bd);

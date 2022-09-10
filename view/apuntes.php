976940ca3d5f36eb91c77d5de5438b9594cdcf39
Carlos <carlos.vicente.mellinas@gmail.com>
2022-09-10 17:18:06 +0200

    Se añaden nuevos apuntes, y se numeran todos los apuntes

--- view/apuntes.php
+++ view/apuntes.php
@@ -42,117 +42,142 @@
     <iframe src="" id="visor"></iframe>
     <div id="apuntes">
         <div class="docs">
-            <h4>Jota de San Antón</h4>
+            <h4>1. Jota de San Antón</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota San Anton.pdf" download="Jota_San_Anton.pdf"><i class="fas fa-download"></i></a>
         </div>
         <div class="docs">
-            <h4>Jota de Sax</h4>
+            <h4>2. Jota de Sax</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota Sax.pdf" download="Jota_Sax.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Salinas</h4>
+            <h4>3. Jota de Salinas</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota Salinas.pdf" download="Jota_Salinas.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Ball de Tres (Pinoso)</h4>
+            <h4>4. Ball de Tres (Pinoso)</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Ball de Tres (Pinoso).pdf" download="Ball_de_Tres.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Danza de Beneixama</h4>
+            <h4>5. Danza de Beneixama</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Danza Beneixama.pdf" download="Danza_Benexiama.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Banyeres</h4>
+            <h4>6. Jota de Banyeres</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota de Banyeres.pdf" download="Jota_Banyeres.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Monovar</h4>
+            <h4>7. Jota de Monovar</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota de Monovar.pdf" download="Jota_Monovar.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Alguenya</h4>
+            <h4>8. Jota de Alguenya</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota Alguenya.pdf" download="Jota_Alguenya.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>L'u d' Alcoi</h4>
+            <h4>9. L'u d' Alcoi</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/L'u d'Alcoi.pdf" download="Lu_Alcoi.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Villena</h4>
+            <h4>10. Jota de Villena</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota de Villena.pdf" download="Jota_Villena.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Fandango de Tres Xativa</h4>
+            <h4>11. Fandango de Tres Xativa</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Fandango de Tres Xativa.pdf" download="Fandango_Xativa.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Novelda</h4>
-            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
-            <a href="/apuntes/Jota Novelda.pdf" download="Jota_Novelda.pdf"><i class="fas fa-download"></i></a> 
-        </div>
-        <div class="docs">
-            <h4>Jota del Postiguet</h4>
-            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
-            <a href="/apuntes/Jota del Postiguet.pdf" download="Jota_Postiguet.pdf"><i class="fas fa-download"></i></a> 
-        </div>
-        <div class="docs">
-            <h4>Bolero Pla de Xàtiva</h4>
+            <h4>12. Bolero Pla de Xàtiva</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Bolero Pla de Xativa.pdf" download="Bolero_Pla_Xativa.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Xàtiva</h4>
+            <h4>13. Jota de Xàtiva</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota de Xativa.pdf" download="Jota_Xativa.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Malaguenya de Barxeta</h4>
-            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
-            <a href="/apuntes/Malaguenya de Barxeta.pdf" download="Malaguenya_Barxeta.pdf"><i class="fas fa-download"></i></a> 
-        </div>
-        <div class="docs">
-            <h4>Jota de Beniganim</h4>
-            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
-            <a href="/apuntes/Jota de Beniganim.pdf" download="Jota_Beniganim.pdf"><i class="fas fa-download"></i></a> 
-        </div>
-        <div class="docs">
-            <h4>Bolero de Castellon</h4>
+            <h4>15. Bolero de Castellon</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Bolero de Castellon.pdf" download="Bolero_Castellon.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Malaguenya de Aspe</h4>
+            <h4>16. Jota del Postiguet</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Jota del Postiguet.pdf" download="Jota_Postiguet.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>17. Jota de Novelda</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Jota Novelda.pdf" download="Jota_Novelda.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>18. Malaguenya de Barxeta</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Malaguenya de Barxeta.pdf" download="Malaguenya_Barxeta.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>19. Jota de Beniganim</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Jota de Beniganim.pdf" download="Jota_Beniganim.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>21. Malaguenya de Aspe</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Malaguenya de Aspe.pdf" download="Malaguenya_Aspe.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Fandango de Xixona</h4>
+            <h4>22. Fandango de Xixona</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Fandango de Xixona.pdf" download="Fandango_Xixona.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de Aspe</h4>
+            <h4>23. Petenera de Xativa</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
-            <a href="/apuntes/Jota de Aspe.pdf" download="Jota_Aspe.pdf"><i class="fas fa-download"></i></a> 
+            <a href="/apuntes/Petenera de Xativa.pdf" download="Petenera_Xativa.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Jota de la Calle de la Cruz</h4>
+            <h4>24. Jota de la Calle de la Cruz</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Jota de la Calle de la Cruz.pdf" download="Jota_Calle_Cruz.pdf"><i class="fas fa-download"></i></a> 
         </div>
         <div class="docs">
-            <h4>Malagueña de la Casa de los Pavos</h4>
+            <h4>25. L'u de Biar</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/L' u de Biar.pdf" download="LU_Biar.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>26. Jota del Castellut</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Jota del Castellut.pdf" download="Jota_Castellut.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>27. Ball de Tres de Monforte</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Ball de Tres de Monforte.pdf" download="Ball_Tres_Monforte.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>28. Fandango de Verdegas</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Fandango de Verdegas.pdf" download="Fandango_Verdegas.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>29. Jota de Aspe</h4>
+            <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
+            <a href="/apuntes/Jota de Aspe.pdf" download="Jota_Aspe.pdf"><i class="fas fa-download"></i></a> 
+        </div>
+        <div class="docs">
+            <h4>30. Malagueña de la Casa de los Pavos</h4>
             <a onclick="mostrar(this)"><i class="fas fa-eye"></i></a>
             <a href="/apuntes/Malaguenya de la Casa de los Pavos.pdf" download="Malaguenya_Casa_Pavos.pdf"><i class="fas fa-download"></i></a> 
         </div>
